3e73109795a0f7d67d6d133ecf0f0b21e77fade6
Pablo-101 <souhail.hajib@gmail.com>
2026-02-08 18:02:21 +0100

    Changing (Password) program to (Sum CSV Column Numbers). Array File

--- Array.py
+++ Array.py
@@ -1,29 +1,14 @@
-done = False
+import csv
+score = 0
+column = 2
 
-while True:
-    new_pass = input("Enter your new password: ")
+with open("csv_file.csv", "r") as file:
+    content = csv.reader(file)
 
-    if len(new_pass) < 6:
-        print("Password can't be less than 6 characters.")
-        continue
-    if len(new_pass) > 12:
-        print("Password can't be more than 12 characters.")
-        continue
-    if not new_pass.isalpha():
-        print("Password can contain only letters.")
-        continue
+    header = next(content)
 
-    # Confirmation loop with option to re-enter the new password
-    while True:
-        confirm_pass = input("Confirm your new password: ")
-        if new_pass == confirm_pass:
-            print("Password set successfully!")
-            done = True      # mark success
-            break            # break inner loop
-        print("Passwords don't match.")
-        choice = input("Type 'c' to try confirming again or 'n' to enter a new password: ").lower()
-        if choice == 'n':
-            break            # break inner loop and go back to outer loop
+    for line in content:
+        line = float(line[column])
+        score += line
 
-    if done:
-        break                # break outer loop because password was set
+print(f"The sum of the 'Score' column is: {score}")
